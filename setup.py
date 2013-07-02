8864f05ed61ec750b6ac1dcccec9b7bb1fff75a4
Christophe Pettus <cpettus@pgexperts.com>
2013-07-02 14:31:22 +0200

    Setup.py.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import django_pglocks
+
+def get_long_description():
+    """
+    Return the contents of the README file.
+    """
+    try:
+        return open('README.rst').read()
+    except:
+        pass  # Required to install using pip (won't have README then)
+
+setup(
+    name = 'django-pglocks',
+    version = django_pglocks.__version__,
+    description = "django_pglocks provides useful context managers for advisory locks for PostgreSQL.",
+    long_description = get_long_description(),
+    author = "Christophe Pettus",
+    author_email = "xof@thebuild.com",
+    license = "PostgreSQL",
+    url = "https://github.com/Xof/django-pglocks",
+    packages = [
+        'django_pglocks',
+    ],
+    package_data = {
+        'facetools': ['templates/facetools/facebook_redirect.html'],
+    },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: PostgreSQL License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Testing',
+    ]
+)
