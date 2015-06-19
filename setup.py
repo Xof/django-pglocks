62daa4f27481b9340d2cb458b115a2e8ce840ca9
Nesan Waran <nesan@counsyl.com>
2015-06-19 13:53:51 -0700

    Inform PyPI of download url and README location

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     author_email = "xof@thebuild.com",
     license = "MIT",
     url = "https://github.com/Xof/django-pglocks",
+    download_url = 'https://github.com/Xof/django-pglocks/tarball/1.0.2',
     packages = [
         'django_pglocks',
     ],
