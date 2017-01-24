6f348ebfb8456a498c5a5af8e34d492b7fd366a9
Alexander Olekhnovich <a.olekhnovich@gmail.com>
2017-01-24 13:38:35 +0100

    Add possibility to check if advisory lock was acquired already

--- django_pglocks/__init__.py
+++ django_pglocks/__init__.py
@@ -1,35 +1,12 @@
 __version__ = '1.0.2'
 
-from contextlib import contextmanager
+from contextlib import contextmanager, closing
 from zlib import crc32
+from django.db import DEFAULT_DB_ALIAS, connections
+from django.utils import six
 
-@contextmanager
-def advisory_lock(lock_id, shared=False, wait=True, using=None):
 
-    from django.db import DEFAULT_DB_ALIAS, connections, transaction
-    from django.utils import six
-
-    if using is None:
-        using = DEFAULT_DB_ALIAS
-
-    # Assemble the function name based on the options.
-
-    function_name = 'pg_'
-
-    if not wait:
-        function_name += 'try_'
-
-    function_name += 'advisory_lock'
-
-    if shared:
-        function_name += '_shared'
-
-    release_function_name = 'pg_advisory_unlock'
-    if shared:
-        release_function_name += '_shared'
-
-    # Format up the parameters.
-
+def format_lock_id(lock_id):
     tuple_format = False
 
     if isinstance(lock_id, (list, tuple,)):
@@ -50,6 +27,33 @@ def advisory_lock(lock_id, shared=False, wait=True, using=None):
             lock_id -= 2**31
     elif not isinstance(lock_id, six.integer_types):
         raise ValueError("Cannot use %s as a lock id" % lock_id)
+
+    return tuple_format, lock_id
+
+
+@contextmanager
+def advisory_lock(lock_id, shared=False, wait=True, using=None):
+    if using is None:
+        using = DEFAULT_DB_ALIAS
+
+    # Assemble the function name based on the options.
+
+    function_name = 'pg_'
+
+    if not wait:
+        function_name += 'try_'
+
+    function_name += 'advisory_lock'
+
+    if shared:
+        function_name += '_shared'
+
+    release_function_name = 'pg_advisory_unlock'
+    if shared:
+        release_function_name += '_shared'
+
+    # Format up the parameters.
+    tuple_format, lock_id = format_lock_id(lock_id)
 
     if tuple_format:
         base = "SELECT %s(%d, %d)"
@@ -80,3 +84,24 @@ def advisory_lock(lock_id, shared=False, wait=True, using=None):
             cursor.execute(command)
 
         cursor.close()
+
+
+def advisory_lock_acquired(lock_id, using=None):
+    if using is None:
+        using = DEFAULT_DB_ALIAS
+
+    with closing(connections[using]) as connection, \
+            closing(connection.cursor()) as cursor:
+        tuple_format, lock_id = format_lock_id(lock_id)
+
+        if tuple_format:
+            where_clause = "classid::bigint = %d AND objid::bigint = %d"
+            params = (lock_id[0], lock_id[1],)
+        else:
+            where_clause = "(classid::bigint << 32) | objid::bigint = %d"
+            params = (lock_id,)
+
+        command = "SELECT 1 FROM pg_locks WHERE %s LIMIT 1" % (where_clause % params)
+        cursor.execute(command)
+
+        return bool(cursor.fetchone())
