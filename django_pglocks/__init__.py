5bee353989bb3fe8e029179d807d77c1bedac879
Chronial <git@chronial.de>
2019-12-28 11:51:35 +0100

    Remove dependency on six

--- django_pglocks/__init__.py
+++ django_pglocks/__init__.py
@@ -1,13 +1,19 @@
 __version__ = '1.0.4'
 
+import sys
 from contextlib import contextmanager
 from zlib import crc32
 
 @contextmanager
 def advisory_lock(lock_id, shared=False, wait=True, using=None):
-
-    import six
     from django.db import DEFAULT_DB_ALIAS, connections, transaction
+
+    if sys.version_info[0] == 2:
+        integer_types = (int, long)
+        string_type = basestring
+    else:
+        integer_types = (int, )
+        string_type = str
 
     if using is None:
         using = DEFAULT_DB_ALIAS
@@ -36,11 +42,11 @@ def advisory_lock(lock_id, shared=False, wait=True, using=None):
         if len(lock_id) != 2:
             raise ValueError("Tuples and lists as lock IDs must have exactly two entries.")
 
-        if not isinstance(lock_id[0], six.integer_types) or not isinstance(lock_id[1], six.integer_types):
+        if not isinstance(lock_id[0], integer_types) or not isinstance(lock_id[1], integer_types):
             raise ValueError("Both members of a tuple/list lock ID must be integers")
 
         tuple_format = True
-    elif isinstance(lock_id, six.string_types):
+    elif isinstance(lock_id, string_type):
         # Generates an id within postgres integer range (-2^31 to 2^31 - 1).
         # crc32 generates an unsigned integer in Py3, we convert it into
         # a signed integer using 2's complement (this is a noop in Py2)
@@ -48,7 +54,7 @@ def advisory_lock(lock_id, shared=False, wait=True, using=None):
         lock_id = (2**31 - 1) & pos
         if pos & 2**31:
             lock_id -= 2**31
-    elif not isinstance(lock_id, six.integer_types):
+    elif not isinstance(lock_id, integer_types):
         raise ValueError("Cannot use %s as a lock id" % lock_id)
 
     if tuple_format:
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email = "xof@thebuild.com",
     license = "MIT",
     url = "https://github.com/Xof/django-pglocks",
-    install_requires = ['six>=1.0.0'],
+    install_requires = [],
     packages = [
         'django_pglocks',
     ],
